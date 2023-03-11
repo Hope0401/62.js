da7eaacd24508b908c1ec65a4574a938f179cf25
Hope0401 <113833937+Hope0401@users.noreply.github.com>
2023-03-11 23:33:52 +0300

    Create 62.js

---
+++ 62.js
@@ -0,0 +1,50 @@
+
+//Задание 1
+//Напишите функцию ucFirst(str), возвращающую строку str с заглавным первым символом.
+//Вам понадобятся методы строк.
+
+function ucFirst(str);
+let newucFirst = 's';
+console.log (newucFirst.toUpperCase(0) + ucFirst);
+
+
+function ucFirst(str);
+console.log (ucFirst.toUpperCase(0) + ucFirst);
+
+//Задание 2
+//Напишите функцию checkSpam(str), возвращающую true, если str содержит 'badWord' или 'XXX', а иначе false.
+//Функция должна быть нечувствительна к регистру.
+
+function checkSpam(str);
+if( checkSpam.includes ('badWord' || 'XXX') {
+console.log ('true');
+} else {
+console.log ('false');
+}
+
+//Задание 3
+//Написать функцию, которой на вход подается строка, на выход она дает символы наоборот (разворачивает строку). Пример: «привет Женя» -> «янеЖ тевирп»
+//Обратите внимание: метод reverse существует только у массивов.
+const ucFirst = ['Даша'];
+ucFirst.reverse();
+console.log (reverse);
+
+//Задание 4
+//Массив содержит строки с информацией о железнодорожных станциях на севере Англии. Строки представляют собой трёхбуквенный код станции, затем некоторые 
+//машиночитаемые данные, за которыми следует точка с запятой, а затем название станции, пригодное для чтения человеком. 
+let stations = [
+'MAN675847583748sjt567654;Manchester Piccadilly',
+'GNF576746573fhdg4737dh4;Greenfield',
+'LIV5hg65hd737456236dch46dg4;Liverpool Lime Street',
+'SYB4f65hf75f736463;Stalybridge',
+'HUD5767ghtyfyr4536dh45dg45dg3;Huddersfield'
+];
+let name1 = stations.slise (0, 4);
+let name2 = stations.slise (0, 4);
+let name3 = stations.slise (0, 4);
+let name4 = stations.slise (0, 4);
+let name5 = stations.slise (0, 4);
+
+
+
+
